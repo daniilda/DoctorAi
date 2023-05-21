fa787b0030d67299d151edb2637a31b463b6dca2
karisDev <karjaakirill@gmail.com>
2023-05-22 01:33:17 +0300

    update svg declaration

--- ui/src/shared/components/Heading/index.tsx
+++ ui/src/shared/components/Heading/index.tsx
@@ -1,7 +1,12 @@
+import Logo from "@/assets/logo.svg";
+
 const Heading = () => {
+  console.log(Logo);
   return (
     <nav className="h-16 bg-bg-nav flex justify-center">
-      <div className="max-w-screen-max w-full p-2 flex items-center"></div>
+      <div className="max-w-screen-max w-full p-2 flex items-center">
+        <Logo width="120" />
+      </div>
     </nav>
   );
 };
