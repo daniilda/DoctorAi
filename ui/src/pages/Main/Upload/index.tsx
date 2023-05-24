55c1339bdb72c6092ec07eb9193fd6c52879326d
karisDev <karjaakirill@gmail.com>
2023-05-24 19:33:32 +0300

    update upload logic

--- ui/src/pages/Main/Upload/index.tsx
+++ ui/src/pages/Main/Upload/index.tsx
@@ -1,11 +1,12 @@
 import Logo from "@/assets/logo.svg";
 import DragDropFile from "./components/DragDropFile";
 import { observer } from "mobx-react-lite";
-import { UploadStore } from "@/stores";
+import { UploadStore } from "./upload.vm";
 import Preview from "./components/Preview";
-import { CSSTransition } from "react-transition-group";
+import { useViewModel } from "@/utils/useViewModel";
 
 const UploadPage = observer(() => {
+  const vm = useViewModel(() => new UploadStore());
   return (
     <main className="flex flex-col items-center min-h-full">
       <Logo className="max-w-md mt-20" />
@@ -14,11 +15,10 @@ const UploadPage = observer(() => {
         данных из медицинской карточки пациента
       </p>
       <div className="flex flex-col mt-16 w-full max-w-lg items-center">
-        <DragDropFile onUpload={UploadStore.addFiles} />
+        <DragDropFile onUpload={vm.addFiles} />
       </div>
       <p className="text-center mt-6 mb-8">поддерживются форматы docx и xlsx</p>
-
-      <Preview />
+      <Preview vm={vm} />
     </main>
   );
 });
